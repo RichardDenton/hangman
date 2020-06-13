f4e244638737315a3ece4195b8e249b9d751015f
Richard Denton <richdenton@gmail.com>
2020-06-13 11:50:29 +0100

    Added colours to gallows

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,11 @@
+require_relative 'colours'
+
 def display_gallows(used_guesses)
-  puts " ________"
-  puts "  |    \\|"
-  puts "  #{used_guesses > 0 ? 'O' : ' '}     |"
-  puts " #{used_guesses > 2 ? '-' : ' '}#{used_guesses > 1 ? '|' : ' '}#{used_guesses > 3 ? '-' : ' '}    |"
-  puts " #{used_guesses > 4 ? '/' : ' '} #{used_guesses > 5 ? '\\' : ' '}    |"
-  puts "        |"
-  puts "==========="
+  puts " ________".yellow
+  puts "  #{'|'.cyan}    #{'\\|'.yellow}"
+  puts "  #{used_guesses > 0 ? 'O'.white : ' '}     #{'|'.yellow}"
+  puts " #{used_guesses > 2 ? '-'.white : ' '}#{used_guesses > 1 ? '|'.white : ' '}#{used_guesses > 3 ? '-'.white : ' '}    #{'|'.yellow}"
+  puts " #{used_guesses > 4 ? '/'.white : ' '} #{used_guesses > 5 ? '\\'.white : ' '}    #{'|'.yellow}"
+  puts "        |".yellow
+  puts "===========".yellow
 end
